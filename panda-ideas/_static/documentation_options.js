6d13e643fc4deb59a7adc5c20220cac87dc50189
coretl <coretl@users.noreply.github.com>
2023-02-28 10:06:48 +0000

    deploy: f27ae12da786381f3c9ebe7c412bdb6d2c796ad8

--- panda-ideas/_static/documentation_options.js
+++ panda-ideas/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev51+gd21cccd',
+    VERSION: '0.1.dev52+gf27ae12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
