4b64487c1f5f524d4a7969c98c720ba45e90d60b
coretl <coretl@users.noreply.github.com>
2023-03-20 12:10:51 +0000

    deploy: 03e47db9879633406bfadb5953901bc349c58b8e

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev49+g3f87aa5',
+    VERSION: '0.1.dev52+g03e47db',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
