0d2ce2d9dfd58d9f7ce25ca303780c47092d0d2e
coretl <coretl@users.noreply.github.com>
2023-05-02 10:25:47 +0000

    deploy: edf110d0e5ecee72862535fada5814d48a9b7351

--- new-signal/_static/documentation_options.js
+++ new-signal/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev53+gd5df0e8',
+    VERSION: '0.1.dev54+gedf110d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
