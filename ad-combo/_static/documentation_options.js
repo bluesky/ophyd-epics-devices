48f0d0b7542bdcd968c072357bebd2d70b371de7
coretl <coretl@users.noreply.github.com>
2023-06-29 13:46:10 +0000

    deploy: 2722b9d5d83ae54ebdfe07a01072549846d509d3

--- ad-combo/_static/documentation_options.js
+++ ad-combo/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev61+gcb22cd1',
+    VERSION: '0.1.dev61+g2722b9d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
