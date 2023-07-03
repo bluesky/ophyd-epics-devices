1530ab27c6fc5151cb15fac33f58faf670c30ab4
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-07-03 10:12:11 +0000

    deploy: d150ad056e35de2d473f3f97d929c329e3143a5a

--- panda-ideas/_static/documentation_options.js
+++ panda-ideas/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev74+gfeab3f7',
+    VERSION: '0.1.dev78+gd150ad0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
