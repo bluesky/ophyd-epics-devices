efea4923c585499c8d45ffcd5a7abe15f12c050e
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-08-01 12:45:59 +0000

    deploy: 9275fc6e0f7f6d4a21f66b99bd7bd8ad703111d1

--- dynamic-panda-signals/_static/documentation_options.js
+++ dynamic-panda-signals/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev83+g4db4fa8',
+    VERSION: '0.1.dev84+g9275fc6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
