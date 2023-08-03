c0f44760113f8e249e41eba2f2041a6463f802ed
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-08-03 15:32:29 +0000

    deploy: 9b244adc57b0148deb303b99954dc48544387733

--- add_zebra/_static/documentation_options.js
+++ add_zebra/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev84+g344de06',
+    VERSION: '0.1.dev85+g9b244ad',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
