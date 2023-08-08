f70e02b7870318d7887748fa231c6b4fb671e9c6
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-08-08 09:10:17 +0000

    deploy: c56980a32bc6365aad885b2b84f293f8688dbcff

--- fix_segfault/_static/documentation_options.js
+++ fix_segfault/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev84+ge69eb6f',
+    VERSION: '0.1.dev85+gc56980a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
