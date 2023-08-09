d519da191d53d5d71edf936891c2ef952b752839
olliesilvester <olliesilvester@users.noreply.github.com>
2023-08-09 10:04:54 +0000

    deploy: 2ed483b5592de177ef93b0af23b394472c00d4e0

--- get_real_panda_connected/_static/documentation_options.js
+++ get_real_panda_connected/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev97+gac8cb11',
+    VERSION: '0.1.dev96+g2ed483b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
