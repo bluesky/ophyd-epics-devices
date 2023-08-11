22469ed8b445878390ce8ddbacfe9c312f35a48c
dmgav <dmgav@users.noreply.github.com>
2023-08-11 20:00:08 +0000

    deploy: e19de492e5ee92ac42eec348cd673cd8ae1645e9

--- set_blocks_and_change_regex/_static/documentation_options.js
+++ set_blocks_and_change_regex/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev96+g9c6984a',
+    VERSION: '0.1.dev97+ge19de49',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
