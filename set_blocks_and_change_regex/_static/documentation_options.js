e6fb45d43704e3f0c37f34e69b7bcdc49252222f
RAYemelyanova <RAYemelyanova@users.noreply.github.com>
2023-08-18 14:44:43 +0000

    deploy: 9b7f653c99bbd201e71c0a540ee4300e1559534b

--- set_blocks_and_change_regex/_static/documentation_options.js
+++ set_blocks_and_change_regex/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev98+g9e0ea0f',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev99+g9b7f653',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
